9321f53c3632d6814b517b90f8ec56416de5c886
omkar2810 <omkargurjar00@gmail.com>
2018-10-20 07:20:33 +0530

    commit4

---
+++ sort.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define pb push_back
+#define mp make_pair
+		
+int main(){
+
+ios::sync_with_stdio(false);
+
+int n;
+cin>>n;
+
+int arr[100];
+for(int i=0;i<n;i++)cin>>arr[i];
+sort(arr,arr+n);
+
+return 0;
+
+}	
